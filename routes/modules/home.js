85f98f697ca44db130e7c282875ae371200a2402
mush1200 <mush1200@hotmail.com>
2021-08-18 21:49:39 +0800

    add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,14 +3,19 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
-
+const User = db.Todo
 // 定義首頁路由
 router.get('/', (req, res) => {
-   return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error('user not found')
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { userId: req.user.id }
+      })
+    })
+    .then((todos) => { return res.render('index', { todos }) })
     .catch((error) => { return res.status(422).json(error) })
 })
 
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,11 +4,11 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-
 router.get('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+  return Todo.findOne({ where: { id, UserId }})
+    .then(todo => res.render('detail', { todo: todo }))
     .catch(error => console.log(error))
 })
 
