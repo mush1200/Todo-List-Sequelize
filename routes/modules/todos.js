60f8d8963ae2ddbfa6ae63207b724851857dddba
mush1200 <mush1200@hotmail.com>
2021-08-18 22:18:21 +0800

    add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,14 +4,27 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+
+//新增一筆資料
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
 router.get('/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
   return Todo.findOne({ where: { id, UserId }})
-    .then(todo => res.render('detail', { todo: todo }))
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
-
 
 // 匯出路由模組
 module.exports = router
